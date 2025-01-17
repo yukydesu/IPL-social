72ae6cceb69c934300e798133e318ab553423174
yukydesu <kylliandelbauchep@gmail.com>
2025-01-17 16:39:46 +0100

    add first test

---
+++ spec/aapSpec.js
@@ -0,0 +1,8 @@
+import { Main } from "../src/app.js";
+
+    it("should return true", function () {
+      // act
+      let result = true
+      // assert
+      expect(result).toBe(true); 
+    });
