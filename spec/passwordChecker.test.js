e7c0cc3ebc5b1decf54ec4f5c5b3082d02c96e43
yukydesu <kylliandelbauchep@gmail.com>
2025-01-17 17:03:46 +0100

    refractor: package.json & create first function  and first methode

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,8 +1,25 @@
 import { Main } from "../src/passwordChecker.js";
 
+describe("Test for the function verifyUniqueName", () => {
+  const main = new Main();
+
     it("should return true", function () {
       // act
       let result = true
       // assert
       expect(result).toBe(true); 
     });
+
+    // 1)	La fonction doit vérifier qu’une chaine de caractère est bien un mot de passe valide :
+    //Il doit contenir au moins 8 charactères.
+
+    it("should return false if password is less than 8 characters", () => {
+      // arrange
+      let password = "1234567";
+      // act
+      let result = main.isPasswordValid(password);
+      // assert
+      expect(result).toBe(false);
+    });
+
+});
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,5 +1,13 @@
 export class Main {
 
+        // 1)	La fonction doit vérifier qu’une chaine de caractère est bien un mot de passe valide :
+    //Il doit contenir au moins 8 charactères.
+    
 
+    isPasswordValid(password) {
+        return false;
 
+    }
 }
+
+export default Main;
